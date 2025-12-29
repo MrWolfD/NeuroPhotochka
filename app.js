6b3a16182e79ea9f40ef4565c25a7bc21fa14e34
MrWolfD <mr12volkoff@gmail.com>
2025-12-30 01:11:13 +0400

    Update app.js

--- app.js
+++ app.js
@@ -396,13 +396,13 @@ function renderPrompts() {
                 <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
                 <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
               </svg>
-              <span class="stat-value" data-stat="copies">${prompt.copies}</span>
+              <span data-stat="copies">${prompt.copies}</span>
             </div>
             <div class="stat-item" title="Добавлено в избранное">
               <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                 <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
               </svg>
-              <span class="stat-value" data-stat="favorites">${prompt.favorites}</span>
+              <span data-stat="favorites">${prompt.favorites}</span>
             </div>
           </div>
           <div class="prompt-actions">
